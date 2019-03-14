55ed4bca710247ad3529f4504a8f3ba10dbc90f5
Wei Lun <weilun_95@hotmail.com>
2019-03-14 14:05:20 +0800

    feat: added cloud functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  firebaseConfig = {
+    apiKey: 'AIzaSyCXenDFrC2ahriYim0K0XA4l_GLOtLngWU',
+    authDomain: 'qbank-d2679.firebaseapp.com',
+    databaseURL: 'https://qbank-d2679.firebaseio.com',
+    projectId: 'qbank-d2679',
+  };
   fileName: string;
+
+  onFileChange(file: any) {
+    const reader = new FileReader();
+
+    // @ts-ignore
+    if (event.target.files && event.target.files.length) {
+      // @ts-ignore
+      const [file] = event.target.files;
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        console.log(reader.result);
+      };
+    }
+  }
 }
 
