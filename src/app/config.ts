1d538ba701c2b177fe9a56d7f7c2ef952f0f02f0
Wei Lun <weilun_95@hotmail.com>
2019-03-14 16:47:09 +0800

    feat: added config fiel

---
+++ src/app/config.ts
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCXenDFrC2ahriYim0K0XA4l_GLOtLngWU',
+  authDomain: 'qbank-d2679.firebaseapp.com',
+  databaseURL: 'https://qbank-d2679.firebaseio.com',
+  projectId: 'qbank-d2679',
+  functionUrl: 'https://us-central1-qbank-d2679.cloudfunctions.net/qsfReader/upload',
+};
